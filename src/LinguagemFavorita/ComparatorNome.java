46309547ca06e057a7e176e00bc5d5679c4c0c1b
Silas <silasmelo12@gmail.com>
2022-11-06 12:27:38 -0300

    Linguagem favorita

---
+++ src/LinguagemFavorita/ComparatorNome.java
@@ -0,0 +1,10 @@
+package LinguagemFavorita;
+
+import java.util.Comparator;
+
+public class ComparatorNome implements Comparator<LinguagemFavorita> {
+    @Override
+    public int compare(LinguagemFavorita linguagemFavorita1, LinguagemFavorita linguagemFavorita2) {
+        return linguagemFavorita1.getNome().compareToIgnoreCase(linguagemFavorita2.getNome());
+    }
+}
