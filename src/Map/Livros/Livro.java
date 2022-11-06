3674383a640af92f6eeb471700094981a902361e
Silas <silasmelo12@gmail.com>
2022-11-06 19:23:14 -0300

    Livros primeiro commit.

--- src/Map/Livros/Livro.java
+++ src/Map/Livros/Livro.java
@@ -1,4 +1,27 @@
 package Map.Livros;
 
 public class Livro {
+    private String nome;
+    private Integer paginas;
+
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
 }
---
+++ src/Map/Livros/OrdenarLivro.java
@@ -0,0 +1,18 @@
+package Map.Livros;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrdenarLivro {
+
+    Map<String,Livro> livros = new HashMap<String, Livro>(){{
+        put("Hawking, Stephen",new Livro("Uma breve história do tempo",200));
+        put("Duhigg, Charles",new Livro("O poder do hábito",408));
+        put("Harari, Yuval Noah",new Livro("21 lições para o século 21",432));
+
+    }};
+//    System.out.println("ordem aleatoria");
+//    System.out.println("ordem de insercao");
+//    System.out.println("ordem natural (Nome)"); //  ordenar pela quantidad ede caracteres do nome
+}
