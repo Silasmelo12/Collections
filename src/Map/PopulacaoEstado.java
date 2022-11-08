e59f0e7d7fddfcb0d1081464dc5560c09dd38a5b
Silas <silasmelo12@gmail.com>
2022-11-07 22:31:45 -0300

    Confere se o estado PB está na lista, se não, adiciona: PB = 4039277

--- src/Map/PopulacaoEstado.java
+++ src/Map/PopulacaoEstado.java
@@ -6,19 +6,38 @@ import java.util.Set;
 
 public class PopulacaoEstado {
 
-    Map<String,Long> populacao = new LinkedHashMap<String, Long>(){{
-        put("PE",9616621l);
-        put("AL",3351543l);
-        put("CE",9187103l);
-        put("RN",3534265l);
+    Map<String, Long> populacao = new LinkedHashMap<String, Long>() {{
+        put("PE", 9616621l);
+        put("AL", 3351543l);
+        put("CE", 9187103l);
+        put("RN", 3534265l);
     }};
 
-    public void manipulador(){
-        populacao.replace("RN",3534165l);
+    public void manipulador() {
+        System.out.println("Lista: ");
         Set<Map.Entry<String, Long>> entries = populacao.entrySet();
-        entries.forEach(l->{
-            System.out.println(l.getKey()+" = "+l.getValue());
+        entries.forEach(l -> {
+            System.out.println(l.getKey() + " = " + l.getValue());
         });
+
+        System.out.println("substituir valor RN para 3534165");
+
+        populacao.replace("RN", 3534165l);
+
+        entries = populacao.entrySet();
+        entries.forEach(l -> {
+            System.out.println(l.getKey() + " = " + l.getValue());
+        });
+
+        System.out.println("Confira se o estado PB está na lista, se não, adicione: PB = 4039277");
+        if(!populacao.containsKey("PB")){
+            populacao.put("PB",4039277l);
+        }
+        entries = populacao.entrySet();
+        entries.forEach(l -> {
+            System.out.println(l.getKey() + " = " + l.getValue());
+        });
+
     }
 
 }
